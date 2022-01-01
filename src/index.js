32119c99a6a9a55747fdfad87f40583bda6dacf5
Mwelwa Nkuta <mwelwankuta@gmail.com>
2022-01-01 13:04:07 +0200

    awaiting async funtions

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ export const server = http.createServer(async (req, res) => {
     const tokenUrl = `https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}&redirect_uri=http://localhost:09644/login`;
     const token = await getAccessToken(tokenUrl);
 
-    if (token.includes('access_token'))
+    if (token.includes('access_token')) {
         accessToken = token.split('=')[1]
+    }
 
-    if (accessToken)
-        sendTokenToClient(accessToken);
+    if (accessToken.length > 0) {
+        await sendTokenToClient(accessToken);
+    }
+
     res.writeHead(200, {
         'Content-Type': 'text/html'
     }).end('<h4 align="center">You can now close your browser :)</h4>')
@@ -29,7 +32,7 @@ async function getAccessToken(tokenUrl) {
 }
 
 async function sendTokenToClient(token) {
-    await fetch(`http://localhost:09644?token=${token}`)
+    return await fetch(`http://localhost:09644?token=${token}`).then(() => console.log('Token sent to client'));
 }
 
 const PORT = process.env.PORT || 7439;
